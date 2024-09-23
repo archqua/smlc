407f5f9b252e21fc8af03140f5a068be5b4bbb4a
Petros Osetros <petr@osetr.lake>
2024-09-24 04:28:35 +1100

    synchronizing docs

--- docgen.sh
+++ docgen.sh
@@ -2,23 +2,24 @@
 COMMIT_MSG=${1:-synchronizing docs}
 pdoc -t pdoc_template -o docs smlc &&
 git add -A &&
+pre-commit run --files docs &&
 git commit -m "$COMMIT_MSG"
 repeat_commit=$?
 if (( repeat_commit != 0 )); then
-  echo repeat commit 1/3 \(trailing whitespace\) &&
+  echo repeat commit \(trailing whitespace\) &&
   git add -A &&
   git commit -m "$COMMIT_MSG"
   repeat_commit=$?
 fi
-if (( repeat_commit != 0 )); then
-  echo repeat commit 2/3 \(black\) &&
-  git add -A &&
-  git commit -m "$COMMIT_MSG"
-  repeat_commit=$?
-fi
-if (( repeat_commit != 0 )); then
-  echo repeat commit 3/3 \(docformatter\) &&
-  git add -A &&
-  git commit -m "$COMMIT_MSG"
-  repeat_commit=$?
-fi
+# if (( repeat_commit != 0 )); then
+#   echo repeat commit 2/3 \(black\) &&
+#   git add -A &&
+#   git commit -m "$COMMIT_MSG"
+#   repeat_commit=$?
+# fi
+# if (( repeat_commit != 0 )); then
+#   echo repeat commit 3/3 \(docformatter\) &&
+#   git add -A &&
+#   git commit -m "$COMMIT_MSG"
+#   repeat_commit=$?
+# fi
