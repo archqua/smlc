51c3a87fce4726e84140e113619fe4c50f5a0e0b
Petros Osetros <petr@osetr.lake>
2024-09-24 04:37:09 +1100

    synchronizing docs

--- docgen.sh
+++ docgen.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 COMMIT_MSG=${1:-synchronizing docs}
 pdoc -t pdoc_template -o docs smlc && (
-pre-commit run --files docs/smlc/*
-git add -A &&
-git commit -m "$COMMIT_MSG"
+  pre-commit run --files docs/smlc/*
+  git add -A &&
+  git commit -m "$COMMIT_MSG"
 )
 # repeat_commit=$?
 # if (( repeat_commit != 0 )); then
